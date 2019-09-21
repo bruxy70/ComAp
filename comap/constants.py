9b9bdc9c6f7f74f8062e4e1eeb6fff2df50dbc95
bruxy70 <44951610+bruxy70@users.noreply.github.com>
2019-09-21 22:51:45 +0200

    Formatting

--- comap/constants.py
+++ comap/constants.py
@@ -5,7 +5,7 @@ Text constants for ComAp API
 URL = {
     'authenticate': 'https://api.websupervisor.net/v1/authenticate',
     'units':   'https://api.websupervisor.net/v1/units',
-    'values' : 'https://api.websupervisor.net/v1/units/{}/values',
+    'values':  'https://api.websupervisor.net/v1/units/{}/values',
     'info':    'https://api.websupervisor.net/v1/units/{}/info',
     'history': 'https://api.websupervisor.net/v1/units/{}/history',
     'files':   'https://api.websupervisor.net/v1/units/{}/files',
